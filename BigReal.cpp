463e7666d520e543ccb83624649ddf1be7850fa9
Mariem-Nabil <146209385+Mariem-Nabil@users.noreply.github.com>
2023-10-31 19:45:37 +0200

    Start put our code

---
+++ BigReal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by n_o_s on 10/31/2023.
+//
+
+#include "BigReal.h"
---
+++ BigReal.h
@@ -0,0 +1,14 @@
+//
+// Created by n_o_s on 10/31/2023.
+//
+
+#ifndef OOP_BIGREAL_H
+#define OOP_BIGREAL_H
+
+
+class BigReal {
+
+};
+
+
+#endif //OOP_BIGREAL_H
